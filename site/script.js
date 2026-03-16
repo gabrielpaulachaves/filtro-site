5f7f6b3974b353ab2ddd934c1767b3aa0c788c88
Gabriel <bielpchaves@gmail.com>
2026-03-16 11:41:44 -0300

    site com filtro

---
+++ site/script.js
@@ -0,0 +1,16 @@
+const fpreto = document.getElementById('fpreto')
+const fbranco = document.getElementById('fbranco')
+const fcorrida = document.getElementById('fcorrida') 
+const fcasual = document.getElementById('fcasual')
+
+const preto = document.getElementsByClassName('preto')
+const branco = document.getElementsByClassName('branco')
+const corrida = document.getElementsByClassName('corrida')
+const casual = document.getElementsByClassName('casual')
+
+let filtro = [
+    {preto},
+    {branco},
+    {corrida},
+    {casual}
+]
