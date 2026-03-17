7e07873102ce2047e1b308b4a9f7a72fbabb68a1
Gabriel <bielpchaves@gmail.com>
2026-03-17 11:28:06 -0300

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,14 +1,4 @@
-const filtrobotao = document.querySelectorAll('.filtro')
-const filtrocartoes = [...document.querySelectorAll('.cartao')]
 
-const cartaofiltrado = ()=> {
-  
-   
-}
-
-console.log(filtrobotao, filtrocartoes)
-
-selectfiltro.forEach((filtro) => filtro.addEventListener('change', cartaofiltrado))
 
 
 
