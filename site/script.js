c1abf95ca1ac3d8f72ca1840cdacafc8adcddd23
Gabriel <bielpchaves@gmail.com>
2026-03-18 21:14:43 -0300

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,6 +1,27 @@
-let filtro = document.querySelectorAll('.filtro')
+let conteiner = document.getElementById('cartoes')
+let filtro = [...document.querySelectorAll('.filtro')]
 let cartao = [...document.querySelectorAll('.cartao')]
 
+function aplicarfiltro(){
+
+    let cor = filtro[0].value
+    let tipo = filtro[1].value
+
+    const listafiltrada = cartao.filter(f => {
+        const info = cartao.getAttibute('data-name').ToLowerCase()
+
+        const corsel = cor === '' || info.includes(cor)
+        const tiposel = tipo === '' || info.includes(tipo)
+
+        return corsel && tiposel
+    })
+    conteiner.innerHTML = ""
+    listafiltrada.forEach(cadabloco => {
+        conteiner.appendChild(cartao)
+    })
+}
+
+filtro.forEach(c => c.addEventListener('change', aplicarfiltro))
 
 
 
